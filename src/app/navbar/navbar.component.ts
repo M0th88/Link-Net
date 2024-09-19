6e3d15779437d2360d2398241f984e7abb05346b
M0th88 <seba_ftw@hotmail.com>
2024-09-18 23:58:48 -0300

    cambios en el navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component,  } from '@angular/core';
+import { Component, HostListener,OnInit } from '@angular/core';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isSticky: boolean = false;
 
-  isOpen = false
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const navbarHeight = document.querySelector('nav')?.clientHeight || 0;
+    this.isSticky = window.pageYOffset > navbarHeight;
+  }
 }
-
